ce9a9af4f31d63c42e7ada225edd10810f21fa3f
Gill Abada <gill@callifi.com>
2019-04-01 14:03:42 -0400

    complete lecture.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-model.js');
@@ -18,6 +19,9 @@ server.get('/', (req, res) => {
 server.post('/api/register', (req, res) => {
   let user = req.body;
 
+  // hash the pass
+  user.password = bcrypt.hashSync(user.password, 10);
+
   Users.add(user)
     .then(saved => {
       res.status(201).json(saved);
@@ -33,7 +37,7 @@ server.post('/api/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -44,7 +48,33 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+function restricted(req, res, next) {
+  const { username, password } = req.headers;
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        next();
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'You must send some creds!' });
+    });
+}
+
+function only(username) {
+  return function(req, res, next) {
+    if (req.headers.username === username) {
+      next();
+    } else {
+      res.status(403).json({ message: `Only ${username} can enter!` });
+    }
+  };
+}
+
+server.get('/api/users', restricted, only('frodo'), (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
